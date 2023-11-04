4eb766988a1d5b45a2b09a62c62b2f77a5cf11a2
gondola <dr.wheats@gmail.com>
2023-11-04 00:05:10 -0400

    slow

--- app/src/main/java/com/example/testingmatrixapi1/MainActivity.java
+++ app/src/main/java/com/example/testingmatrixapi1/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonSubmitOnClick(View view) {
+        TextView textViewFirstName = findViewById(R.id.textViewFirstName);
+        TextView textViewLastName = findViewById(R.id.textViewLastName);
+        TextView textViewEmail = findViewById(R.id.textViewEmail);
+
+        EditText editTextFirstName = findViewById(R.id.textFieldFirstName);
+        EditText editTextLastName = findViewById(R.id.textFieldLastName);
+        EditText ediTextEmail = findViewById(R.id.textFieldEmail);
+
+        textViewFirstName.setText("First Name: " +  editTextFirstName.getText());
+        textViewLastName.setText("First Name: " +  editTextLastName.getText());
+        textViewEmail.setText("First Name: " +  ediTextEmail.getText());
+
     }
 }
